e0df737973913f7bf732d82f6b3e5997b3df5fe8
Miracle1048 <miraclematthias1@gmail.com>
2024-06-10 13:39:35 +0100

    Create and update object

--- week2/object.js
+++ week2/object.js
@@ -1,2 +1,15 @@
 // const date = new Date();
-// console.log(date);
+
+// const month = ['january', 'fabuary'] 
+
+const table = {
+    noOfLegs: 4,
+    color: 'white', 
+    material:'pastic',
+    isFoldable:true,
+}
+console.log(table.noOfLegs);
+
+table['coloroflegs']= 'grey';
+table['width'] = 1.2;
+console.log(table.coloroflegs + '\n ' + table.material +  '\n ' + table.width)
