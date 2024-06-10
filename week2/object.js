63c7dc1d59660acf5529b4e4f9dfadeb7cd2a2d1
Miracle1048 <miraclematthias1@gmail.com>
2024-06-10 14:07:30 +0100

    Learn to loop through objects

--- week2/object.js
+++ week2/object.js
@@ -1,6 +1,7 @@
 // const date = new Date();
 
-// const month = ['january', 'fabuary'] 
+// const month = ['january', 'fabuary', 'march']
+//console.log(date.getUTCMonth()); 
 
 const table = {
     noOfLegs: 4,
@@ -8,8 +9,37 @@ const table = {
     material:'pastic',
     isFoldable:true,
 }
-console.log(table.noOfLegs);
+
+const fan = {
+    color: 'black',
+    type:'standing',
+    isRechargeable: false,
+    noOfBlades: 3,
+    adjustableHeight: true,
+}
+
+const television = {}
+television['type'] = 'flatscreen'
+television['size'] = 55;
+television['color'] = 'silver'
+television['maker'] = 'Startimes'
+// console.log(table.noOfLegs);
 
 table['coloroflegs']= 'grey';
 table['width'] = 1.2;
-console.log(table.coloroflegs + '\n ' + table.material +  '\n ' + table.width)
+// console.log(table.coloroflegs + '\n ' + table.material +  '\n ' + table.width)
+
+for (item in table){
+    console.log(table[item]);
+}
+console.log('\n\n')
+
+for (properties in fan){
+    console.log(fan[properties])
+}
+
+console.log('\n\n')
+
+for (item in television){
+    console.log(television[item])
+}
