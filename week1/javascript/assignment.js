2d5785814a6298d7661034c7dd1b2a5edcd14ce9
Miracle1048 <miraclematthias1@gmail.com>
2024-06-10 13:24:32 +0100

    learn about date and math objects

---
+++ week1/javascript/assignment.js
@@ -0,0 +1,6 @@
+//learning how to save a function in a variable
+const calcArea = function (i, j){
+    return i * j
+}
+let result = calcArea(5,5)
+alert(result)
---
+++ week1/javascript/function.js
@@ -0,0 +1,29 @@
+// function sayHello(name){
+//     console.log('hello ' + name);
+// }
+
+//     sayHello('Miracle')
+//     sayHello('Code campus')
+//     sayHello('Nigeria')
+//     sayHello('Africa')
+//     sayHello('michael')
+
+//     function square(number){
+//         return number * number;
+//         return answer;
+//     }
+
+//     alert(square(5));
+//     console.log(square(5));
+//     document.getElementById('display').innerHTML = square(5);
+
+
+//Assignment
+//save a functions in a varaiable
+
+// const x = function(a,b){
+//     return a*b
+// };
+// let z = x(4,3)
+// console.log(z)
+
